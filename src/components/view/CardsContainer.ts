038f339e24be3e0d82ffc4f3dcbd3416f303cf83
Kseniya Buyanova <YuleCat25@yandex.ru>
2025-09-18 21:56:56 +0300

    Var 1

---
+++ src/components/view/CardsContainer.ts
@@ -0,0 +1,17 @@
+import { View } from "../base/View";
+
+interface ICardsContainer {
+  catalog: HTMLElement[];
+}
+
+export class CardsContainer extends View<ICardsContainer> {
+  protected _catalog: HTMLElement;
+
+  // constructor(container: HTMLElement) {
+  //   super(container);
+  // }
+
+  set catalog(items: HTMLElement[]) {
+    this.container.replaceChildren(...items);
+  }
+}
